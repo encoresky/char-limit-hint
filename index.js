b2e2041e6066bbaa9d0562c6e7382ebd5ce73bb3
Vishal Chaturvedi <vc121356@gmail.com>
2021-11-15 14:03:27 +0530

    Iniial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+Node.prototype.charLimit = function (limit) {
+
+  if(this.nodeName !== "TEXTAREA" && this.nodeName !== "INPUT") {
+
+    var invalidElement = document.createElement("span");
+    invalidElement.classList.add("invalid-element");
+    invalidElement.innerText = "Only input element is allowed";  
+    this.after(invalidElement);
+
+    return 0;
+
+  }
+
+  if(this.type !== "textarea" && this.type !== "text") {
+
+    var invalidElementType = document.createElement("span");
+    invalidElementType.classList.add("invalid-element-type");
+    invalidElementType.innerText = "Only text and textarea input type is allowed";
+    this.after(invalidElementType);
+
+    return 0;
+  }
+
+  var hint = document.createElement("span");
+  hint.classList.add("char-limit-hint");
+  hint.innerText = limit + " characters remaining";
+
+  this.after(hint);
+  this.maxLength = limit;
+
+  function handleMouseUp(event) {
+    var text_remaining = limit - event.target.value.length;
+    hint.innerHTML = text_remaining + " characters remaining";
+  }
+
+  this.addEventListener("keyup", handleMouseUp, true);
+};
+
+var NodeCharElement = document.querySelector.bind(document);
